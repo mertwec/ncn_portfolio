704172db903aa796afbdabcc3133a0b9a8359a4b
Валентин Олейников <crm.26188@dz.com>
2023-04-26 14:17:24 +0300

    fix header responses to tesmanian

--- news/scraping/tools/tool_requests.py
+++ news/scraping/tools/tool_requests.py
@@ -6,10 +6,10 @@ from typing import Union
 
 headers = {
     'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-    #   'Mozilla/5.0(X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0 Chrome/103.0.5060.134 Safari/537.36'
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate, br',
+    #             'Mozilla/5.0(X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0 Chrome/103.0.5060.134 Safari/537.36'
+    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    # 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    # 'Accept-Encoding': 'gzip, deflate, br',
 }
 
 proxies = {
